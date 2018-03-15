5f3a38311525690fc621ddab9946d38a9394f955
dxw <xianwdong@163.com>
2018-03-15 15:04:46 +0800

    添加No.58

---
+++ 58. Length of Last Word.java
@@ -0,0 +1,24 @@
+// Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+// return the length of last word in the string.
+// If the last word does not exist, return 0.
+// Note: A word is defined as a character sequence consists of non-space characters only.
+// For example, 
+// Given s = "Hello World",
+// return 5.
+
+public int lengthOfLastWord(String s) {
+    int length = 0;
+    for (int i = s.length() - 1; i >= 0; --i) {
+        if (Character.isLetter(s.charAt(i))) {
+            for (int j = i; j >= 0; --j) {
+                if (Character.isLetter(s.charAt(j))) {
+                    ++length;
+                } else {
+                    break;
+                }
+            }
+            return length;
+        }
+    }
+    return length;
+}
