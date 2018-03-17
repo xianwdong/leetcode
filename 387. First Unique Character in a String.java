8293d2d462b8bb9cd6a3a4979ba81ea896b8263c
dxw <xianwdong@163.com>
2018-03-17 23:14:25 +0800

    添加No.387

---
+++ 387. First Unique Character in a String.java
@@ -0,0 +1,20 @@
+/*Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+Examples:
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.*/
+
+public int firstUniqChar(String s) {
+    int[] times = new int[26];
+    for (int i = 0; i < s.length(); ++i) {
+        ++times[s.charAt(i) - 'a'];
+    }
+    for (int i = 0; i < s.length(); ++i) {
+        if (times[s.charAt(i) - 'a'] == 1) {
+            return i;
+        }
+    }
+    return -1;
+}
